194f26c1d28471f339283dc43467a65b26d0d1af
Brent Williams <jbwtech@gmail.com>
2021-06-16 17:07:18 -0400

    Added activate and deactivate functions.

--- import-remote-attachments.php
+++ import-remote-attachments.php
@@ -13,6 +13,16 @@ defined( 'ABSPATH' ) OR exit;
  * add_filter( 'http_request_host_is_external', '__return_true' );
  */
 
+function activate_import-remote-attachments() {
+   /* create table of allowed hosts */
+}
+register_activation_hook( __FILE__, 'activate_import-remote-attachments' );
+
+function deactivate_import-remote-attachments() {
+   /* delete table of allowed hosts */
+}
+register_deactivation_hook( __FILE__, 'deactivate_import-remote-attachments' );
+
 add_filter( 'http_request_host_is_external', 'allow_my_custom_host', 10, 3 );
 
 function allow_my_custom_host( $allow, $host, $url ) {
