602416417c542a4b8abefac88fb3355fc42db17d
Brent Williams <jbwtech@gmail.com>
2021-06-16 21:46:18 -0400

    Added activation and deactivation functions as well as code to cleanup the table created

--- import-remote-attachments.php
+++ import-remote-attachments.php
@@ -1,12 +1,13 @@
 <?php
 defined( 'ABSPATH' ) OR exit;
 /**
- * Plugin Name:  Import Remote Attachments
- * Plugin URI:   https://github.com/jbwtech/import-remote-attachments
- * Description:  A Wordpress plugin to override default behavior of not allowing importing attachments from remote URL's.
- * Version:      0.0.2
- * Author:       Brent Williams
- * Author URI:   https://github.com/jbwtech/
+ * Plugin Name:        Import Remote Attachments
+ * Plugin URI:         https://github.com/jbwtech/import-remote-attachments
+ * Description:        A Wordpress plugin to override default behavior of not allowing importing attachments from remote URL's.
+ * Version:            0.0.2
+ * Requires at least:  3.5
+ * Author:             Brent Williams
+ * Author URI:         https://github.com/jbwtech/
  */
 
 /*
@@ -15,11 +16,13 @@ defined( 'ABSPATH' ) OR exit;
 
 function activate_import_remote_attachments() {
    /* create table of allowed hosts */
+   ira_install();
 }
 register_activation_hook( __FILE__, 'activate_import_remote_attachments' );
 
 function deactivate_import_remote_attachments() {
    /* delete table of allowed hosts */
+   ira_cleanup();
 }
 register_deactivation_hook( __FILE__, 'deactivate_import_remote_attachments' );
 
@@ -31,6 +34,36 @@ function allow_my_custom_host( $allow, $host, $url ) {
   if ( preg_match( $exp, $host ) ) 
     $allow = true;
   return $allow;
+}
+
+function ira_install() {
+     
+   global $wpdb;
+
+   $charset_collate = $wpdb->get_charset_collate();
+   $table_name = $prefix . "import_allowed_hosts";
+
+   $sql = "CREATE TABLE $table_name (
+      id mediumint(9) NOT NULL AUTO_INCREMENT,
+      slug varchar(75) NOT NULL,
+      hostname varchar(75) NOT NULL,
+      PRIMARY KEY (id)
+   ) $charset_collate;";
+
+   require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+   dbDelta( $sql );
+}
+
+function ira_cleanup() {
+
+   global $wpdb;
+
+   $table_name = $prefix . "import_allowed_hosts";
+
+   $sql = "DROP TABLE $table_name;";
+
+   require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+   dbDelta( $sql );
 }
 
 ?>
