68f2f4ba22f3e80b1f8eb3d730130825b56fda0a
Brent Williams <jbwtech@gmail.com>
2021-06-23 16:04:57 -0400

    Update options.php
    output formatting

--- options.php
+++ options.php
@@ -1,9 +1,6 @@
 <?php
 namespace EclipseComp\IRA;
 
-echo '<div class="wrap">';
-echo '<h1>Import Remote Attachments</h1>';
-
 add_action( 'admin_menu', 'EclipseComp\IRA\menu' );
 
 function menu() {
@@ -15,6 +12,8 @@ function import_remote_attachments_options() {
    if ( !current_user_can( 'manage_options' ) ) {
       wp_die( __( 'You do not have sufficient permissions to access this page.') );
    }
+   echo '<div class="wrap">';
+   echo '<h1>Import Remote Attachments</h1>';
    echo '<p>Here is where the formwould go if I actually had options.</p>';
    echo '</div>';
 }
