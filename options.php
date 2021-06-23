fed2f3143a0c1e2c9659f261ca3dc5cde208bde6
Brent Williams <jbwtech@gmail.com>
2021-06-23 16:10:12 -0400

    Update options.php
    Changed callable function reference to include namespace

--- options.php
+++ options.php
@@ -5,10 +5,10 @@ add_action( 'admin_menu', 'EclipseComp\IRA\menu' );
 
 function menu() {
    add_options_page( 'Import Remote Attachments Options', 'Import Remote Attachments', 'manage_options',
-      'import-remote-attachments-options', 'import_remote_attachments_options');
+      'import-remote-attachments-options', 'EclipseComp\IRA\options');
 }
 
-function import_remote_attachments_options() {
+function options() {
    if ( !current_user_can( 'manage_options' ) ) {
       wp_die( __( 'You do not have sufficient permissions to access this page.') );
    }
