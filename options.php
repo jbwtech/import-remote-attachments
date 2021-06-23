d6a3674741a29db20150b8b2803f50c0795bb1d6
Brent Williams <jbwtech@gmail.com>
2021-06-23 16:57:24 -0400

    Update options.php

--- options.php
+++ options.php
@@ -1,23 +1,19 @@
 <?php
 namespace EclipseComp\IRA;
 
-add_action( 'network_admin_menu', 'EclipseComp\IRA\ira_menu' );
+add_action( 'network_admin_menu', 'EclipseComp\IRA\menu' );
 
-function ira_menu() {
-   add_submenu_page( 'settings.php', 'Import Remote Attachments Options', 'Import Remote Attachments', 'manage_options', 'import-remote-attachments', 'EclipseComp\IRA\ira_options' );
-/*
-   add_options_page( __('Import Remote Attachments Options', 'textdomain'), __('Import Remote Attachments', 'textdomain'), 'manage_options',
-      'import-remote-attachments.php', 'EclipseComp\IRA\ira_options');
-*/
+function menu() {
+   add_submenu_page( 'settings.php', 'Import Remote Attachments Options', 'Import Remote Attachments', 'manage_options', 'import-remote-attachments', 'EclipseComp\IRA\options' );
 }
 
-function ira_options() {
+function options() {
    if ( !current_user_can( 'manage_options' ) ) {
       wp_die( __( 'You do not have sufficient permissions to access this page.') );
    }
    echo '<div class="wrap">';
    echo '<h1>Import Remote Attachments</h1>';
-   echo '<p>Here is where the formwould go if I actually had options.</p>';
+   echo '<p>Here is where the form would go if I actually had options.</p>';
    echo '</div>';
 }
 
