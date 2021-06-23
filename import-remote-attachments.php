437e249ef8c337a6131927ad36c12443cd616587
Brent Williams <jbwtech@gmail.com>
2021-06-23 16:42:15 -0400

    moved back to options.php file

--- import-remote-attachments.php
+++ import-remote-attachments.php
@@ -67,21 +67,4 @@ function cleanup() {
    require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
    $wpdb->query( $sql );
 }
-
-add_action( 'admin_menu', 'EclipseComp\IRA\ira_menu' );
-
-function ira_menu() {
-   add_options_page( __('Import Remote Attachments Options', 'textdomain'), __('Import Remote Attachments', 'textdomain'), 'manage_options',
-      'import-remote-attachments.php', 'EclipseComp\IRA\ira_options');
-}
-
-function ira_options() {
-   if ( !current_user_can( 'manage_options' ) ) {
-      wp_die( __( 'You do not have sufficient permissions to access this page.') );
-   }
-   echo '<div class="wrap">';
-   echo '<h1>Import Remote Attachments</h1>';
-   echo '<p>Here is where the formwould go if I actually had options.</p>';
-   echo '</div>';
-}
 ?>
